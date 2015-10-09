bfdcdccfe4966364adfd3852b4fc21d1b4693574
Erik Krogen <erikkrogen@gmail.com>
2015-10-08 21:06:48 -0700

    Added an initial implementation of bond annotations, without much bond core, including tests

--- pybond/bond/bond.py
+++ pybond/bond/bond.py
@@ -1,9 +1,74 @@
-"""
-Hello
-"""
+from functools import wraps
+import inspect
+
+TESTING = True # False # should be False by default but leaving as True during initial development
 
 class Bond:
-    """
-    Bond class
-    """
-    pass
+
+    NO_MOCK_RESPONSE = '_bond_no_mock_response' # TODO ?
+
+    _instance = None
+    @staticmethod
+    def instance():
+        if Bond._instance is None:
+            Bond._instance = Bond()
+        return Bond._instance
+
+    def __init__(self):
+        self.pointReturns = {}
+
+    def pushObserverReturn(self, spyPointName, returnValue):
+        self.pointReturns[spyPointName] = returnValue
+
+    def observe(self, spyPointName, formatter=None, **kwargs):
+        observationDictionary = kwargs if formatter is None else formatter(kwargs)
+        print "{}: {}".format(spyPointName, observationDictionary)
+        if spyPointName in self.pointReturns:
+            return self.pointReturns[spyPointName]
+        return Bond.NO_MOCK_RESPONSE
+    
+    def getOutputDirectoryPath(self):
+        return '/tmp/bondOutput'
+
+# Function annotation for observation
+""" Internal Notes:
+    - wouldn't make sense to pass an observe function here since that puts the 'mock' inside of prod code
+      - maybe would make sense to have a single default return value to return during testing?
+    - this MUST be the first decorator applied to your function (i.e. the bottommost one) or else
+      the inspect.getargspec() won't work
+"""
+# Dependency injection for mocking out bond for testing?
+def observeFunction(spyPointName=None, mockMandatory=False, excludedKeys=(), formatter=None, observeReturn=False, bond=Bond.instance()):
+    assert TESTING, 'Must be in testing mode to use observations!'
+
+    def wrap(fn):
+        pointName = fn.__name__ if spyPointName is None else spyPointName
+        arginfo = inspect.getargspec(fn)
+        # print arginfo
+
+        @wraps(fn)
+        def fnWrapper(*args, **kwargs):
+            observationDictionary = {}
+            for idx, arg in enumerate(args):
+                if idx != 0 or arginfo.args[idx] != 'self':  # TODO more rigorous check?
+                    observationDictionary[arginfo.args[idx]] = arg
+            for key, val in kwargs.iteritems():
+                observationDictionary[key] = val
+            observationDictionary = {key: val for (key, val) in observationDictionary.iteritems()
+                                     if key not in excludedKeys and key != 'self'}
+
+            response = bond.observe(pointName, formatter=formatter, **observationDictionary)
+            if mockMandatory:
+                assert response is not Bond.NO_MOCK_RESPONSE, 'You MUST mock out spypoint {}'.format(pointName)
+            if response is Bond.NO_MOCK_RESPONSE:
+                retVal = fn(*args, **kwargs)
+                # if observeReturn:
+                    # TODO would be nice for these two observations to be on the same output. maybe could have
+                    # some sort of 'observePartial' that keeps saves info but doesn't log it yet, waits for an
+                    # 'observeComplete' or something
+                    # bond.observe(pointName + '.return', retVal)
+                return retVal
+            else:
+                return response
+        return fnWrapper
+    return wrap
---
+++ pybond/tests/bond_annotation_tests.py
@@ -0,0 +1,90 @@
+import unittest
+from bond import bond
+
+
+class BondAnnotationTests(unittest.TestCase):
+
+    class BondMock:
+
+        def __init__(self):
+            self.nextReturnValue = None
+
+        def observe(self, spyPointName, formatter=None, **kwargs):
+            self.spyPointName = spyPointName
+            self.observationDictionary = kwargs
+            if self.nextReturnValue is not None:
+                value = self.nextReturnValue
+                self.nextReturnValue = None
+                return value
+            return bond.Bond.NO_MOCK_RESPONSE
+
+        def setNextReturnValue(self, value):
+            self.nextReturnValue = value
+
+        def getLastSpyPointName(self):
+            return self.spyPointName
+
+        def getLastObservationDictionary(self):
+            return self.observationDictionary
+    bondMock = BondMock()
+
+    @bond.observeFunction(bond=bondMock)
+    def annotatedStandardMethod(self, arg1, arg2):
+        return 'return value'
+
+    def testStandardAnnotation(self):
+        self.assertEquals('return value', self.annotatedStandardMethod(1, 2))
+        # These tests later can be replaced with bond.observe but for now use asserts
+        self.assertEqual('annotatedStandardMethod', BondAnnotationTests.bondMock.getLastSpyPointName())
+        self.assertDictEqual({'arg1': 1, 'arg2': 2}, BondAnnotationTests.bondMock.getLastObservationDictionary())
+
+    def testWithMocking(self):
+        BondAnnotationTests.bondMock.setNextReturnValue('mocked value')
+        self.assertEquals('mocked value', self.annotatedStandardMethod(arg1=1, arg2=2))
+        self.assertEqual('annotatedStandardMethod', BondAnnotationTests.bondMock.getLastSpyPointName())
+        self.assertDictEqual({'arg1': 1, 'arg2': 2}, BondAnnotationTests.bondMock.getLastObservationDictionary())
+
+    @staticmethod
+    @bond.observeFunction(bond=bondMock)
+    def annotatedStaticMethod(arg1='foo', arg2='bar'):
+        pass
+
+    def testStaticAnnotation(self):
+        self.annotatedStaticMethod(arg2='bar2')
+        # These tests later can be replaced with bond.observe but for now use asserts
+        self.assertEqual('annotatedStaticMethod', BondAnnotationTests.bondMock.getLastSpyPointName())
+        self.assertDictEqual({'arg2': 'bar2'}, BondAnnotationTests.bondMock.getLastObservationDictionary())
+
+    @bond.observeFunction(spyPointName='testPointName', bond=bondMock)
+    def annotatedWithNewName(self):
+        pass
+
+    def testNewPointName(self):
+        self.annotatedWithNewName()
+        self.assertEqual('testPointName', BondAnnotationTests.bondMock.getLastSpyPointName())
+
+    @bond.observeFunction(bond=bondMock, excludedKeys=('arg2', 'newArg'))
+    def annotatedWithExclusion(self, arg1, arg2, **kwargs):
+        pass
+
+    def testWithExclusion(self):
+        self.annotatedWithExclusion('foo', 'bar', newArg='disappears', newArg2='baz')
+        self.assertDictEqual({'arg1': 'foo', 'newArg2': 'baz'}, BondAnnotationTests.bondMock.getLastObservationDictionary())
+
+    @bond.observeFunction(bond=bondMock, mockMandatory=True)
+    def annotatedWithMockingMandatory(self, arg1=None):
+        return 'return value'
+
+    def testWithMockingMandatoryApplied(self):
+        BondAnnotationTests.bondMock.setNextReturnValue('mocked value')
+        self.assertEqual('mocked value', self.annotatedWithMockingMandatory())
+
+    def testWithMockingMandatoryNotApplied(self):
+        try:
+            self.annotatedWithMockingMandatory()
+            self.fail()
+        except AssertionError:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
