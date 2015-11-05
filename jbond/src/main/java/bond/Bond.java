47b6ffd3762ea10e7816772b6bf6582b49b63ec7
Erik Krogen <erikkrogen@gmail.com>
2015-11-04 18:36:02 -0800

    [jbond] Initial project structure

---
+++ jbond/src/main/java/bond/Bond.java
@@ -0,0 +1,20 @@
+package bond;
+
+public class Bond {
+
+  /**
+   * Private constructor to enforce singleton-style pattern
+   */
+  private Bond() {}
+
+  public static void startTest() {
+
+  }
+
+  public static void spy() {
+
+  }
+
+
+
+}
